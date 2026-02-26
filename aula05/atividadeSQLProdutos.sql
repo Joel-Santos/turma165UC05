7d246a77d3e40001c0f483c20700fdb93c5a23ce
Joel Santos <joeloliveira56@gmail.com>
2026-02-26 19:43:28 -0300

    feat: Aula 05 - Lista SQL

--- aula05/atividadeSQLProdutos.sql
+++ aula05/atividadeSQLProdutos.sql
@@ -54,4 +54,69 @@ values
 ('TV', 30,  3000.25,  2,  2);
 
 
+-- 1. Crie uma regra para garantir que o nome de um 
+-- produto seja único na tabela produtos
+alter table produtos add constraint unique_nome_produto unique(nome);
+
+-- 2. Defina uma constraint para garantir que o preço dos 
+-- produtos nunca seja menor que zero.
+alter table produtos add constraint check_preco check(preco>=0);
+
+insert into produtos(NOME, QUANTIDADE, PRECO, fk_FORNECEDOR ,fk_CATEGORIA)
+values
+('Cadeira azul', 30,  -500 ,5, 5);
+
+
+-- 3. Implemente uma constraint que assegure 
+-- que a coluna quantidade na tabela produtos nunca seja nula.
+
+alter table produtos  alter column quantidade set not null;
+
+
+-- 4. Adicione uma constraint para garantir que o estado dos 
+-- fornecedores sempre siga o padrão de duas letras, como "SP" ou "RJ".
+alter table fornecedores add constraint check_estado check(estado ~'^[A-Z]{2}$');
+
+-- 5. Crie uma constraint para que o nome das categorias na tabela categorias seja único.
+alter table categorias add constraint unique_nome_catagoria unique(nome);
+
+
+-- 6. INSERIR dados de 2 produtos de categoria 3 e qualquer fornecedor
+insert into produtos(id, NOME, QUANTIDADE, PRECO, fk_FORNECEDOR ,fk_CATEGORIA)
+values
+(6, 'Cadeira preta', 30,  300.00 ,4, 3),(7, 'Cadeira rosa', 30,  300.00 ,3, 3);
+
+-- 7. INSERIR dados de 2 fornecedores distintos, 
+-- sendo do Estado do RN e outro do estado da PB;
+insert into fornecedores (ID, NOME, RUA, CIDADE, ESTADO)
+values 
+(6, 'Senac RN', 'Av. Almirante Alexandrino de Alencar', 'Natal',  'RN'),
+(7, 'Senac PB', 'Av. Almirante Alexandrino de Alencar', 'João Pessoa',  'PB');
+
+-- 8. INSERIR dados de mais 1 categoria de nome Nacional
+insert into categorias(nome)
+values('Nacional');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
